068c3798ecabb655bb847495c81fdccc9ce2a940
K <51281148+K-dash@users.noreply.github.com>
2024-09-28 09:51:45 +0000

    feat: Enhance onboarding form with validation and structural improvements
    - "権限ユーザ"のフルネーム編集時の手動変更フラグを追加し、フルネーム変更時の動作を改善
    - 基本情報セクションと契約内容セクションのフィールドセットを更新し、メールアドレス、プラン、利用開始日、利用終了日、および広告形式の項目を追加
    - CustomerInfoクラスにデータアノテーション属性を追加し、入力検証を強化
    - 成功とエラーメッセージの通知機能を追加して、ユーザーフィードバックを改善
    - CustomerEnvironmentsおよびMultipleValuesコンポーネントを新規作成
    - "顧客サマリー"にプランと広告形式の表示を追加
    - スタイルを微調整（例: 境界半径、パディング、色）

--- Components/Pages/OnbordingForm/CustomerInfo.cs
+++ Components/Pages/OnbordingForm/CustomerInfo.cs
@@ -1,9 +1,40 @@
+using System.ComponentModel.DataAnnotations;
+
 public class CustomerInfo
 {
-    public string CompanyName { get; set; }
-    public string FullName { get; set; }
-    public int NumberOfUsers { get; set; }
+    [Display(Name = "会社名")]
+    [Required(ErrorMessage = "会社名を入力してください")]
+    public string? CompanyName { get; set; }
+
+    [Display(Name = "担当者名")]
+    [Required(ErrorMessage = "担当者名を入力してください")]
+    public string? FullName { get; set; }
+
+    [Display(Name = "メールアドレス")]
+    [Required(ErrorMessage = "メールアドレスを入力してください")]
+    [EmailAddress(ErrorMessage ="メールアドレス形式で入力してください")]
+    public string? EmailAddress { get; set; }
+
+    [Display(Name = "利用人数")]
+    [Required(ErrorMessage = "利用人数を入力してください")]
+    [RegularExpression(@"^[1-9]\d*$", ErrorMessage = "1以上の数値を入力してください")]
+    public string? NumberOfUsers { get; set; }
+
+    [Display(Name = "プラン")]
+    [Required(ErrorMessage = "プランを入力してください")]
+    public string? Plan { get; set; }
+
+    [Display(Name = "利用開始日")]
+    [Required(ErrorMessage = "利用開始日を入力してください")]
     public DateTime? StartDate { get; set; }
+
+    [Display(Name = "利用終了日")]
+    [Required(ErrorMessage = "利用終了日を入力してください")]
     public DateTime? EndDate { get; set; }
-    public List<string> authorityUsers {get; set;}
+
+    public List<string>? authorityUsers {get; set;}
+    
+    [Display(Name = "広告形式")]
+    [Required(ErrorMessage = "選択してください")]
+    public List<string> AdvertisingFormats { get; set; }
 }
